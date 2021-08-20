862ac2fda80cbc88c596a6518874cde37a77c893
Sai Pranay <saipranayadepu07@gmail.com>
2021-08-20 16:07:00 +0530

    eb2021 update

--- src/components/pages/Team/Team21.js
+++ src/components/pages/Team/Team21.js
@@ -3,28 +3,23 @@ import { Link } from 'react-router-dom';
 import Header from "../../Header/Header";
 import { headerObjSeventeen } from "../../Header/HeaderData";
 import CC from "./../../../images/team/profasish.jpg";
-import CL from "./../../../images/team/vijay.png";
-import Pres from "./../../../images/team/eb8.png";
-// import CCC from "./../../../images/team/eb2.png";
-// import Chair from "./../../../images/team/eb14.png";
-// import Director from "./../../../images/team/aman.png";
-// import PR from "./../../../images/team/harsha.png";
-// import VP1 from "./../../../images/team/eb8.png";
-// import VP2 from "./../../../images/team/vijay.png";
-// import Sec from "./../../../images/team/ph.png";
-// import Tres from "./../../../images/team/eb13.png";
-// import Tech1 from "./../../../images/team/jay.png";
-// import Tech2 from "./../../../images/team/eb12.png";
-// import Tech3 from "./../../../images/team/eb6.png";
-// import Tech4 from "./../../../images/team/saipranay.png";
-// import Event1 from "./../../../images/team/navya.png";
-// import Event2 from "./../../../images/team/eb3.png";
-// import Design1 from "./../../../images/team/bhavana.png";
-// import Design2 from "./../../../images/team/ph.png";
-// import Design3 from "./../../../images/team/ph.png";
-// import Design4 from "./../../../images/team/saurav.png";
-// import Content1 from "./../../../images/team/chirag.png";
-// import Content2 from "./../../../images/team/ph.png";
+import ph from "./../../../images/team/ph.png";
+import cl from "./../../../images/team/eb21/Vijay_CommunityLead.PNG";
+import cp from "./../../../images/team/eb21/Adithya_President.JPG";
+import vp from "./../../../images/team/eb21/Bhargavi_VicePresident.jpeg";
+import dr from "./../../../images/team/harsha.png";
+import pr from "./../../../images/team/eb21/Tarini_HeadOfPublicRelations.jpg";
+import sc from "./../../../images/team/eb21/Ojasva_Secretary.jpeg";
+import tl1 from "./../../../images/team/eb21/Krishna Raj_TechLead.jpg";
+import tl2 from "./../../../images/team/eb21/Madhav_TechLead.jpg";
+import trl from "./../../../images/team/eb21/SaiPranay_TrackLead.jpg";
+import md from "./../../../images/team/eb21/MaheshDasika_TechDepartment.jpg";
+import bmd from "./../../../images/team/eb21/BindhuMadhav_TechDepartment.jpg";
+import el2 from "./../../../images/team/eb21/Chethan_EventLead.jpg";
+import eo from "./../../../images/team/eb21/Abhiram_HeadOfVideographyAndEventOrg.jpg";
+import om from "./../../../images/team/eb21/Sabareesh_SocialMediaLead.jpg";
+import ce from "./../../../images/team/eb21/chirag_ChiefEditor.jpg";
+import d1 from "./../../../images/team/eb21/Teja _ TechLead+Designer.jpg";
 
 import "./TeamMemberCard.css";
 
@@ -33,9 +28,9 @@ function Team21() {
         <div>
             <Header {...headerObjSeventeen} />
             <div className="board-category1">
-                <div className="category-title">To be announced!</div>
+                <div className="category-title">Core.</div>
             </div>
-{/*<div className="mcard-box">
+            <div className="mcard-box">
                 <div className="mcard-container">
                     <img src={CC} className="mcard-image" alt="ad-img"></img>
                     <div className="mcard-name">Prof. Asish K. Dalai</div>
@@ -50,8 +45,8 @@ function Team21() {
             </div>
             <div className="mcard-box">
                 <div className="mcard-container">
-                    <img src={CL} className="mcard-image" alt="ad-img"></img>
-                    <div className="mcard-name">S Vijay Balaji</div>
+                    <img src={cl} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Vijay Balaji</div>
                     <div className="mcard-title">Community Leader</div>
                     <div className="mcard-flex">
                         <div className="b1"></div>
@@ -63,8 +58,19 @@ function Team21() {
             </div>
             <div className="mcard-box">
                 <div className="mcard-container">
-                    <img src={Pres} className="mcard-image" alt="ad-img"></img>
-                    <div className="mcard-name">G V Datta Adithya</div>
+                    <img src={dr} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">S.V.Harsha Vardhan</div>
+                    <div className="mcard-title">Director</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={cp} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Datta Adithya</div>
                     <div className="mcard-title">President</div>
                     <div className="mcard-flex">
                         <div className="b1"></div>
@@ -73,7 +79,216 @@ function Team21() {
                         <div className="b4"></div>
                     </div>
                 </div>
-            </div>*/}
+                <div className="mcard-container">
+                    <img src={vp} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Bhargavi</div>
+                    <div className="mcard-title">Vice President</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="mcard-box">
+                <div className="mcard-container">
+                    <img src={pr} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Tarini</div>
+                    <div className="mcard-title">Head of Public Relations</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={ph} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Kaarthik</div>
+                    <div className="mcard-title">Chairman</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={sc} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Ojasva</div>
+                    <div className="mcard-title">Secretary</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="board-category2">
+                <div className="category-title">Tech.</div>
+            </div>
+            <div className="mcard-box">
+                <div className="mcard-container">
+                    <img src={tl1} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Krishna Raj</div>
+                    <div className="mcard-title">Technical Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={tl2} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Madhav</div>
+                    <div className="mcard-title">Technical Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={trl} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Sai Pranay</div>
+                    <div className="mcard-title">Track Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+        <div className="mcard-container">
+                    <img src={md} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Mahesh Dasika</div>
+                    <div className="mcard-title">Developer</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+        <div className="mcard-container">
+                    <img src={bmd} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Bindhu madhav</div>
+                    <div className="mcard-title">Developer</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="board-category3">
+                <div className="category-title">Events.</div>
+            </div>
+            <div className="mcard-box">
+                <div className="mcard-container">
+                    <img src={ph} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Maruthi</div>
+                    <div className="mcard-title">Event Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={el2} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Chethan</div>
+                    <div className="mcard-title">Event Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={eo} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Abhiram</div>
+                    <div className="mcard-title">Organizer</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={om} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Sabareesh Reddy</div>
+                    <div className="mcard-title">Outreach Manager</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="board-category4">
+                <div className="category-title">Design.</div>
+            </div>
+            <div className="mcard-box">
+                <div className="mcard-container">
+                    <img src={ce} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Chirag</div>
+                    <div className="mcard-title">Chief Editor</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={eo} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Abhiram</div>
+                    <div className="mcard-title">Head of Videography</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={trl} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Sai Pranay</div>
+                    <div className="mcard-title">Track Lead</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+                <div className="mcard-container">
+                    <img src={d1} className="mcard-image" alt="ad-img"></img>
+                    <div className="mcard-name">Teja</div>
+                    <div className="mcard-title">Designer</div>
+                    <div className="mcard-flex">
+                        <div className="b1"></div>
+                        <div className="b2"></div>
+                        <div className="b3"></div>
+                        <div className="b4"></div>
+                    </div>
+                </div>
+               
+            </div>
+            
         </div>
     )
 }
