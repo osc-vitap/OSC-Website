1eb9c5aa6408ef1675eee41e3fdb52684fed9f3a
ashutosh <ashutoshpatel168@gmail.com>
2022-01-26 15:05:14 +0530

    added the eb openings page

--- src/components/pages/EBOpenings/EBcard.js
+++ src/components/pages/EBOpenings/EBcard.js
@@ -6,11 +6,15 @@ function EBcard(props){
 
 
         <div className="eb-container">
-            <div className="eb-card" data-label="In Progress">
+            <div className="eb-card" data-label={(props.vacancy ? "Position Open" : "No Vacancy")} >
                 <div className="card__container">
-                    <h1 className="card__header">{props.role}</h1>
+                    <h2 className="card__header">{props.role}</h2>
                     <p className="card__body">{props.roled}</p> 
-                    <button type="button" class="button button1"><a href="" target="_blank">Apply Now</a></button>
+                    <div className={(props.vacancy ? 'show' : 'hide')}>
+                    <button type="button" className="button button1" >
+                        <a href="https://osc464741.typeform.com/to/OCxoQGXo" rel="noreferrer" target="_blank">Apply Now</a>
+                    </button>
+                    </div>
                 </div>
             </div>
         </div>
